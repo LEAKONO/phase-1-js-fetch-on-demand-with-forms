772a370e33ee75d3c1f3f1a3ded214eaaa6eacb4
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-04-02 20:53:45 +0300

    Demand-with-forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-const init = () => {
-  
-}
 
-document.addEventListener('DOMContentLoaded', init);
+const init = () => {
+    const inputForm = document.querySelector("form");
+  
+    inputForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const input = document.querySelector("input#searchByID");
+  
+      console.log(input.value);
+  
+      fetch("http://localhost:3000/movies")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          // LOG: (3) [{…}, {…}, {…}]
+        });
+    });
+  };
+  
+  document.addEventListener("DOMContentLoaded", init);
